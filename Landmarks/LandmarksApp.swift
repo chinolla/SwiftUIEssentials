5c83c46b501006b4d2d9b76dc143aec6fc1e7058
Chris Chinolla <chris@MacBook-Pro.lan>
2024-01-07 12:26:36 -0700

    initial commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Chris Chinolla on 1/6/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
