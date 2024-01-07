f31a2ef27eb34d7b647e8b51af5071f6315c77a0
Chris Chinolla <chris@MacBook-Pro.lan>
2024-01-07 13:55:52 -0700

    Creating Dynamic Lists

--- Landmarks/Views/MapView.swift
+++ Landmarks/Views/MapView.swift
@@ -9,15 +9,21 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
-    var body: some View {
-        Map(initialPosition: .region(region))
-    }
-}
+    var coordinate: CLLocationCoordinate2D
 
-private var region: MKCoordinateRegion {
-    MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 34.011_286, longitude:-116.166_868),
-        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+
+    var body: some View {
+            Map(position: .constant(.region(region)))
+        }
+    
+    
+    private var region: MKCoordinateRegion {
+           MKCoordinateRegion(
+               center: coordinate,
+               span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+           )
+       }
+
 }
 
 
@@ -25,5 +31,5 @@ private var region: MKCoordinateRegion {
 
 
 #Preview {
-    MapView()
+    MapView(coordinate: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868))
 }
